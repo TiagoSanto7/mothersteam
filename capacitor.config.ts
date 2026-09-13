18487f8ecad8942194b5b15218f5d5ad4e242e79
TiagoSanto7 <tiagoalvessoares17@gmail.com>
2026-09-12 20:44:33 -0400

    fix(ios): keyboard resize none to prevent viewport zoom

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -12,6 +12,10 @@ const config: CapacitorConfig = {
     PushNotifications: {
       presentationOptions: ['badge', 'sound', 'alert'],
     },
+    Keyboard: {
+      resize: 'none',
+      resizeOnFullScreen: true,
+    },
   },
 };
 
